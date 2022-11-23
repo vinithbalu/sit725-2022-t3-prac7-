2ffabdc5a07850219d212df06ce8cd58979f0892
vinithbalu <rbvinithraja@gmail.com>
2022-11-24 00:21:44 +1100

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express")
+
+var app = express()
+
+const addnumbers = (number1, number2) => {
+    var num1 = parseInt(number1)
+    var num2 = parseInt(number2)
+    var res = num1+num2;
+    return result
+
+}
+
+app.get("/add two numbers", (req,res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = addnumbers(number1, number2)
+    res.json({statuscode: 200, data: result, message: 'success'})
+})
+
+var port = process.env.port || 3000
+
+
+app.listen(port,()=>{
+    console.log("App listening to: http://localhost:"+port)
+
+})
