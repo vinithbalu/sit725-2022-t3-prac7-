9f8d2fa7b9c3425e0711fb7efc5f12264b834414
vinithbalu <rbvinithraja@gmail.com>
2022-11-29 00:00:59 +1100

    updated prac3

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 var express = require("express")
-
 var app = express()
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({extends: false}))
 
-const addnumbers = (number1, number2) => {
-    var num1 = parseInt(number1)
-    var num2 = parseInt(number2)
-    var res = num1+num2;
-    return res
 
-}
+//const addnumbers = (number1, number2) => {...
+//})  
 
-app.get("/addtwonumbers", (req,res) => {
-    var number1 = req.query.number1;
-    var number2 = req.query.number2;
-    var result = addnumbers(number1, number2)
-    res.json({statuscode: 200, data: result, message: 'success'})
-})
-
-var port = process.env.port || 3000
+var port = process.env.port || 3000;
 
 
 app.listen(port,()=>{
